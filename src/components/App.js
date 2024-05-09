f70601e04d9aa9df510c54e7832191205ce2e37e
mshankar0101 <madhushankar0101@gmail.com>
2024-05-10 00:39:02 +0530

    first commit

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import "../styles/App.css";
+import React from "react";
+import Flights from "./Flights";
+
+function App() {
+  return (
+  <div className="App"> 
+      <Flights/>
+  </div>
+  );
+}
+
+export default App;
